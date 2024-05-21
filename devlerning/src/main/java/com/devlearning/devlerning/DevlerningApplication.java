3ffc4ab1b7e87052ff5be56ddcdc9a71f0bb1240
Sillas cavalcanti <sillascavalcanti1@gmail.com>
2024-05-20 23:17:28 -0300

    SpringBoot BackEnd Project start

---
+++ devlerning/src/main/java/com/devlearning/devlerning/DevlerningApplication.java
@@ -0,0 +1,13 @@
+package com.devlearning.devlerning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlerningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlerningApplication.class, args);
+	}
+
+}
---
+++ devlerning/src/test/java/com/devlearning/devlerning/DevlerningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlearning.devlerning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlerningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

72be9916b4cca7194614144a4af0c49c2e33c503
Sillas cavalcanti <sillascavalcanti1@gmail.com>
2024-05-30 00:50:07 -0300

    Remove getters and setters for utilise lombok getters and setters

--- devlerning/src/main/java/com/devlearning/devlerning/entity/Question.java
+++ devlerning/src/main/java/com/devlearning/devlerning/entity/Question.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,49 +21,4 @@ public class Question {
     private String answer3;
     private String answer4;
     private String solution;
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-    public String getAnswer1() {
-        return answer1;
-    }
-    public void setAnswer1(String answer1) {
-        this.answer1 = answer1;
-    }
-    public String getAnswer2() {
-        return answer2;
-    }
-    public void setAnswer2(String answer2) {
-        this.answer2 = answer2;
-    }
-    public String getAnswer3() {
-        return answer3;
-    }
-    public void setAnswer3(String answer3) {
-        this.answer3 = answer3;
-    }
-    public String getAnswer4() {
-        return answer4;
-    }
-    public void setAnswer4(String answer4) {
-        this.answer4 = answer4;
-    }
-    public String getSolution() {
-        return solution;
-    }
-    public void setSolution(String solution) {
-        this.solution = solution;
-    }
-
 }
